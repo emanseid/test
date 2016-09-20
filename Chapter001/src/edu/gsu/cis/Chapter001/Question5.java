35e682178674a3be2d2f50d243d62186db670b11
emanseid <emanseid@gmail.com>
2016-09-20 11:41:29 -0400

    add

---
+++ Chapter001/src/edu/gsu/cis/Chapter001/Question5.java
@@ -0,0 +1,39 @@
+package edu.gsu.cis.Chapter001;
+
+public class Question5 {
+	public static void main(String[]args){
+
+double a;
+double b;
+double c;
+double d;
+double e;
+double f;
+
+a = 9.5;
+b = 4.5;
+c = 2.5;
+d = 3;
+e = 45.5;
+f = 3.5;
+
+double mult1 = (a * b);
+double mult2 = (c * d);
+double sub1 = ((a * b) - (c * d));
+double sub2 = (e - f);
+double div = ((a * b) -(c * d)) /(e - f);
+
+        System.out.println(mult1);
+        System.out.println(mult2);
+        System.out.println(sub1);
+        System.out.println(sub2);
+        System.out.println("Final result = "+div);
+
+	}
+
+
+
+		
+		
+		
+}
